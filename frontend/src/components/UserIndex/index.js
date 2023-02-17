d5c62db0888fa27221a4ad51dcbfd12ad0337945
Will Weihnacht <wwhynot@terpmail.umd.edu>
2023-02-16 17:52:19 -0800

    userPages finished

--- frontend/src/components/UserIndex/index.js
+++ frontend/src/components/UserIndex/index.js
@@ -1,18 +1,26 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../../store/usersReducer";
+import UserIndexItem from "../UserIndexItem";
 import './UserIndex.css';
 
 const UserIndex = () => {
     const dispatch = useDispatch();
-    const users = useSelector(state => state.users);
+    const users = useSelector(state => Object.values(state.users));
 
     useEffect(()=>{
         dispatch(fetchUsers());
     },[])
 
     return (
-        <div></div>
+        <div className="question-feed">
+            <div id="header-box">
+                <h1>All Users</h1>
+            </div>
+            <ul>
+                {users?.map((user,i) => <UserIndexItem user={user} key={i}/>)}
+            </ul>
+        </div>
     )
 }
 
