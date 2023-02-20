abaaa5fd8a745312276fb306a3a1717d96bae04e
Will Weihnacht <wwhynot@terpmail.umd.edu>
2023-02-19 19:30:43 -0800

    fixed footer styling

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -4,23 +4,30 @@ import './Footer.css';
 const Footer = () => {
     return (
         <footer id='footer'>
-            <div className="footer-div">
-                <h4>Socials</h4>
-                <a href='https://www.linkedin.com/in/william-weihnacht/'>LinkedIn</a>
-                <a href="https://github.com/WilliamWeihnacht">GitHub</a>
-                <a href="https://angel.co/u/will-weihnacht">WellFound</a>
-            </div>
-            <div className="footer-div" id="center-footer-div">
-                <h4>Projects</h4>
-                <a href="https://williamweihnacht.github.io/Autobattler-js-project/">Autobattler</a>
-                <a href="https://foodjunkies.onrender.com/">Food Junkies</a>
-                <a href="https://stackoverflowclone-pk3b.onrender.com/">Hack Overload</a>
-            </div>
-            <div className="footer-div">
-                <h4>Links</h4>
-                <Link to={'/login'}>Login</Link>
-                <Link to={'/signup'}>Sign Up</Link>
-                <Link to={'/questions'}>Questions</Link>
+            <div id="footer-content-container">
+                <div className="footer-div">
+                    <div id="big-logo-div">
+                        <p id='ltgt'>&lt;</p><p id='sl'>/</p><p id='ltgt'>&gt;</p>
+                    </div>
+                </div>
+                <div className="footer-div">
+                    <h4>Socials</h4>
+                    <a href='https://www.linkedin.com/in/william-weihnacht/'>LinkedIn</a>
+                    <a href="https://github.com/WilliamWeihnacht">GitHub</a>
+                    <a href="https://angel.co/u/will-weihnacht">WellFound</a>
+                </div>
+                <div className="footer-div" id="center-footer-div">
+                    <h4>Projects</h4>
+                    <a href="https://williamweihnacht.github.io/Autobattler-js-project/">Autobattler</a>
+                    <a href="https://foodjunkies.onrender.com/">Food Junkies</a>
+                    <a href="https://stackoverflowclone-pk3b.onrender.com/">Hack Overload</a>
+                </div>
+                <div className="footer-div">
+                    <h4>Links</h4>
+                    <Link to={'/login'}>Login</Link>
+                    <Link to={'/signup'}>Sign Up</Link>
+                    <Link to={'/questions'}>Questions</Link>
+                </div>
             </div>
         </footer>
     )
